b43bf77cc77bf03456cc0bef571efb9587cff114
yzshen <yzshen@chromium.org>
2015-04-10 14:38:06 -0700

    Mojo message format: add a version field for interfaces.
    BUG=451206
    TEST=Newly-added tests pass.
    R=hansmuller@google.com, jamesr@chromium.org, qsr@chromium.org, rogulenko@google.com, zra@google.com
    
    Review URL: https://codereview.chromium.org/1037223004

--- generators/mojom_cpp_generator.py
+++ generators/mojom_cpp_generator.py
@@ -77,7 +77,9 @@ def GetCppType(kind):
     return "%s_Data*" % GetNameForKind(kind, internal=True)
   if mojom.IsUnionKind(kind):
     return "%s_Data" % GetNameForKind(kind, internal=True)
-  if mojom.IsInterfaceKind(kind) or mojom.IsInterfaceRequestKind(kind):
+  if mojom.IsInterfaceKind(kind):
+    return "mojo::internal::Interface_Data"
+  if mojom.IsInterfaceRequestKind(kind):
     return "mojo::MessagePipeHandle"
   if mojom.IsEnumKind(kind):
     return "int32_t"
@@ -215,7 +217,9 @@ def GetCppFieldType(kind):
   if mojom.IsMapKind(kind):
     return ("mojo::internal::StructPointer<mojo::internal::Map_Data<%s, %s>>" %
             (GetCppType(kind.key_kind), GetCppType(kind.value_kind)))
-  if mojom.IsInterfaceKind(kind) or mojom.IsInterfaceRequestKind(kind):
+  if mojom.IsInterfaceKind(kind):
+    return "mojo::internal::Interface_Data"
+  if mojom.IsInterfaceRequestKind(kind):
     return "mojo::MessagePipeHandle"
   if mojom.IsEnumKind(kind):
     return GetNameForKind(kind)
