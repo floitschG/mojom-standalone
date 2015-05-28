94c454cf26f1b1af748328e87f5d92cdd2379096
Alexandre Zani <azani@chromium.org>
2015-05-28 10:44:20 -0700

    Revert alignment change for unions since it breaks go tests.
    TBR=qsr@chromium.org
    
    Review URL: https://codereview.chromium.org/1162523004

--- pylib/mojom/generate/pack.py
+++ pylib/mojom/generate/pack.py
@@ -61,8 +61,6 @@ class PackedField(object):
   def GetAlignmentForKind(cls, kind):
     if isinstance(kind, mojom.Interface):
       return 4
-    if isinstance(kind, mojom.Union):
-      return 8
     return cls.GetSizeForKind(kind)
 
   def __init__(self, field, index, ordinal):
