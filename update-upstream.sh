c4b1077eeec82186a54910996af0e4fcd0f66548
Florian Loitsch <floitsch@google.com>
2016-02-03 21:54:08 +0100

    Add mojom_parser executables.

--- update-upstream.sh
+++ update-upstream.sh
@@ -7,3 +7,19 @@ git checkout upstream-master
 git subtree split --prefix mojo/public/tools/bindings -b upstream-bindings
 git checkout $current_branch
 git subtree merge --prefix bindings upstream-bindings
+
+# The following downloads require depot_tools.
+download_from_google_storage.py \
+   --no_resume \
+   --quiet \
+   --no_auth \
+   --bucket mojo/mojom_parser/linux64 \
+   -s bindings/mojom_parser/bin/linux64/mojom_parser.sha1
+
+download_from_google_storage.py \
+   --no_resume \
+   --quiet \
+   --no_auth \
+   --bucket mojo/mojom_parser/mac64 \
+   -s bindings/mojom_parser/bin/mac64/mojom_parser.sha1
+
