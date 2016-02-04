363406f2c466d0c20ec26b3a2aca20612a3c594e
Florian Loitsch <floitsch@google.com>
2016-02-04 19:10:41 +0100

    Move everything into lib/public.

--- update-upstream.sh
+++ update-upstream.sh
@@ -9,9 +9,9 @@ git subtree split --prefix mojo/public/tools/bindings -b upstream-bindings
 git subtree split --prefix mojo/public/python -b upstream-python
 git subtree split --prefix mojo/public/third_party -b upstream-third_party
 git checkout $current_branch
-git subtree merge -m "merge upstream" --prefix lib/tools/bindings upstream-bindings
-git subtree merge -m "merge upstream" --prefix lib/python upstream-python
-git subtree merge -m "merge upstream" --prefix lib/third_party upstream-third_party
+git subtree merge -m "merge upstream" --prefix lib/public/tools/bindings upstream-bindings
+git subtree merge -m "merge upstream" --prefix lib/public/python upstream-python
+git subtree merge -m "merge upstream" --prefix lib/public/third_party upstream-third_party
 
 # The following downloads require depot_tools.
 download_from_google_storage.py \
@@ -19,12 +19,12 @@ download_from_google_storage.py \
    --quiet \
    --no_auth \
    --bucket mojo/mojom_parser/linux64 \
-   -s lib/tools/bindings/mojom_parser/bin/linux64/mojom_parser.sha1
+   -s lib/public/tools/bindings/mojom_parser/bin/linux64/mojom_parser.sha1
 
 download_from_google_storage.py \
    --no_resume \
    --quiet \
    --no_auth \
    --bucket mojo/mojom_parser/mac64 \
-   -s lib/tools/bindings/mojom_parser/bin/mac64/mojom_parser.sha1
+   -s lib/public/tools/bindings/mojom_parser/bin/mac64/mojom_parser.sha1
 
