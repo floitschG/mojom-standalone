28af8e7c1014d36a8b9ffcdb37ee9b84de56430c
Florian Loitsch <floitsch@google.com>
2016-02-04 18:26:48 +0100

    Move files into lib and add python directory.

--- update-upstream.sh
+++ update-upstream.sh
@@ -5,8 +5,10 @@ git fetch upstream
 git checkout -b upstream-master --track upstream/master
 git checkout upstream-master
 git subtree split --prefix mojo/public/tools/bindings -b upstream-bindings
+git subtree split --prefix mojo/public/python -b upstream-python
 git checkout $current_branch
-git subtree merge --prefix bindings upstream-bindings
+git subtree merge --prefix lib/tools/bindings upstream-bindings
+git subtree merge --prefix lib/tools/python upstream-python
 
 # The following downloads require depot_tools.
 download_from_google_storage.py \
@@ -14,12 +16,12 @@ download_from_google_storage.py \
    --quiet \
    --no_auth \
    --bucket mojo/mojom_parser/linux64 \
-   -s bindings/mojom_parser/bin/linux64/mojom_parser.sha1
+   -s lib/tools/bindings/mojom_parser/bin/linux64/mojom_parser.sha1
 
 download_from_google_storage.py \
    --no_resume \
    --quiet \
    --no_auth \
    --bucket mojo/mojom_parser/mac64 \
-   -s bindings/mojom_parser/bin/mac64/mojom_parser.sha1
+   -s lib/tools/bindings/mojom_parser/bin/mac64/mojom_parser.sha1
 
